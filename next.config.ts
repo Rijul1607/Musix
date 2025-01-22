f8e3722dde2d22f1637f7bbb7745ee1fc792dcfe
rijul <rijulgulati16@gmail.com>
2025-01-22 20:44:31 +0530

    commit

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: [
-      "images.unsplash.com", // Allow images from Unsplash
+      "images.unsplash.com"// Allow images from Unsplash
     ],
   },
   /* Add any other config options here */
